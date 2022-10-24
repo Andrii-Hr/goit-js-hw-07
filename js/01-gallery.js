07dacf836e2cdde250db7f85b9b3bae9e99e6fbb
Andrii-Hr <andriihryb@ukr.net>
2022-10-24 17:38:10 +0300

    Initial commit

---
+++ js/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+
+function makeGalleryImgMarkup({ preview, original, description }) {
+    return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+        </a>
+    </div>`;
+};
+
+function makeGalleryMarkup(array) {
+    return array.map(makeGalleryImgMarkup).join('');
+};
+
+function renderMarkup(markup) {
+	galleryEl.insertAdjacentHTML('afterbegin', markup);
+};
+
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+renderMarkup(galleryMarkup);
+
+galleryEl.addEventListener('click', galleryImgClick);
+
+function galleryImgClick(element) {
+	if (element.target.nodeName !== 'IMG') {
+		return;
+	}
+	element.preventDefault();
+	const modalOptions = {
+		onShow: () => {
+			window.addEventListener('keydown', onEscapeKeyDown);
+		},
+		onClose: () => {
+			window.removeEventListener('keydown', onEscapeKeyDown);
+		},
+    };
+    
+	const modal = basicLightbox.create(`<img  src="${element.target.dataset.source}"width="1400" height="900">`, modalOptions);
+
+	function onEscapeKeyDown(element) {
+		if (element.code === 'Escape') {
+			modal.close();
+		}
+	}
+	modal.show();
+};
